f8c33581fa691c323f2f08562ca4848033b6508f
Shashank J <shashankreddy231@gmail.com>
2021-02-10 22:27:37 +0530

    Preferences Dialog prettified

---
+++ Swing-36-Arranging-And-Designing-Design-Forms/src/model/Database.java
@@ -0,0 +1,57 @@
+package model;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Database {
+	
+	private LinkedList<Person> people;
+	
+	public Database() {
+		people = new LinkedList<Person>();
+	}
+	
+	public void addPerson(Person person) {
+		people.add(person);
+	}
+	
+	public List<Person> getPeople() {
+		return people;
+	}
+	
+	public void saveToFile(File file) throws IOException {
+		FileOutputStream fos = new FileOutputStream(file);
+		ObjectOutputStream oos = new ObjectOutputStream(fos);
+		
+		Person[] persons = people.toArray(new Person[people.size()]);
+		oos.writeObject(persons);
+		oos.close();
+		
+	}
+	
+	public void loadFromFile(File file) throws IOException {
+		FileInputStream fis = new FileInputStream(file);
+		ObjectInputStream ois = new ObjectInputStream(fis);
+		
+		try {
+			Person[] persons = (Person[]) ois.readObject();
+			people.clear();
+			people.addAll(Arrays.asList(persons));
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		ois.close();
+	}
+
+	public void removePerson(int index) {
+		people.remove(index);
+	}
+}
